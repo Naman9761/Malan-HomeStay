917505e208fb1b2ae05ca393d83781a17e1a76de
Naman joshi <namanjoc9761@gmail.com>
2024-04-24 23:26:36 +0530

    Removed

--- hotel-admin/src/sections/@dashboard/booking/index.js
+++
@@ -1,4 +0,0 @@
-export { default as BookingListHead } from './BookingListHead';
-export { default as BookingListToolbar } from './BookingListToolbar';
-export { default as BookingListBody } from './BookingListBody';
-
--- hotel-admin/src/sections/@dashboard/room/index.js
+++
@@ -1,6 +0,0 @@
-export { default as RoomModel } from './RoomModel';
-export { default as RoomListBody } from './RoomListBody';
-export { default as RoomListToolbar } from './RoomListToolbar';
-export { default as RoomListHead } from './RoomListHead';
-
-
